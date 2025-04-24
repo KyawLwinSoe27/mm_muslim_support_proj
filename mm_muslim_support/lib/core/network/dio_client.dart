de0ec8b947229883931dbb92648ecf1dae078994
Kyaw Lwin Soe <kyawlwinsoe@globalwave.company>
2025-04-24 22:07:30 +0630

    project setup configuration

---
+++ mm_muslim_support/lib/core/network/dio_client.dart
@@ -0,0 +1,73 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:mm_muslim_support/utility/constants.dart'; // for debugPrint
+
+class DioClient {
+  final Dio _dio;
+
+  DioClient()
+      : _dio = Dio(BaseOptions(
+    baseUrl: AppConstants.baseUrl,
+    connectTimeout: const Duration(seconds: 10),
+    receiveTimeout: const Duration(seconds: 10),
+  )) {
+    // Add interceptors
+    _dio.interceptors.add(LogInterceptor(
+      request: true,
+      requestHeader: true,
+      requestBody: true,
+      responseHeader: false,
+      responseBody: true,
+      error: true,
+      logPrint: (obj) => debugPrint(obj.toString()),
+    ));
+
+    // Add error middleware
+    _dio.interceptors.add(InterceptorsWrapper(
+      onError: (DioException e, handler) {
+        _handleError(e);
+        return handler.next(e); // continue
+      },
+    ));
+  }
+
+  void _handleError(DioException error) {
+    if (error.type == DioExceptionType.connectionTimeout) {
+      debugPrint('Connection timeout!');
+    } else if (error.type == DioExceptionType.receiveTimeout) {
+      debugPrint('Receive timeout!');
+    } else if (error.type == DioExceptionType.badResponse) {
+      final status = error.response?.statusCode;
+      final message = error.response?.data.toString();
+      debugPrint('API Error: $status - $message');
+    } else {
+      debugPrint('Unexpected Dio error: ${error.message}');
+    }
+    // You can also throw custom exceptions here
+  }
+
+  Future<Response> get(String path, {Map<String, dynamic>? queryParams, Options? options}) {
+    return _dio.get(path, queryParameters: queryParams, options: options);
+  }
+
+  Future<Response> post(String path, {dynamic data, Options? options}) {
+    return _dio.post(path, data: data, options: options);
+  }
+
+  Future<Response> put(String path, {dynamic data, Options? options}) {
+    return _dio.put(path, data: data, options: options);
+  }
+
+  Future<Response> delete(String path, {dynamic data, Options? options}) {
+    return _dio.delete(path, data: data, options: options);
+  }
+
+  Future<Response> postMultipart(String path, Map<String, dynamic> fields, List<MultipartFile> files,
+      {Options? options}) {
+    final formData = FormData.fromMap({
+      ...fields,
+      'files': files,
+    });
+    return _dio.post(path, data: formData, options: options);
+  }
+}
---
+++ mm_muslim_support/lib/core/routing/context_ext.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
+
+/// A collection of convenient navigation methods extending [BuildContext].
+///
+/// This extension provides methods to handle common navigation tasks such as
+/// popping the current screen, navigating with clearing the back stack, and
+/// pushing named routes with extra arguments.
+
+extension ContextExt on BuildContext {
+  /// Pops the current route off the navigator's stack.
+  ///
+  /// If [result] is provided, it will be returned to the previous route as the
+  /// result of the `pop` operation.
+  ///
+  /// - [T] is the type of the result.
+  /// - [result] is the optional result to return to the previous route.
+  void back<T extends Object?>([T? result]) {
+    return Navigator.of(this).pop(result);
+  }
+
+  /// Clears the entire navigator stack and navigates to the specified route.
+  ///
+  /// This method will clear all routes in the navigator stack and then push
+  /// the specified [routeName]. It is useful for navigating to a new screen
+  /// and ensuring that the user cannot navigate back to the previous screens.
+  ///
+  /// - [routeName] is the name of the route to navigate to.
+  /// - [extra] is an optional object to pass as extra arguments to the new route.
+  void navigateAndClearStack(String routeName, {Object? extra}) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      while (Navigator.of(this).canPop()) {
+        back();
+      }
+      pushReplacementNamed(routeName, extra: extra);
+    });
+  }
+
+  void navigateAndClearStackWithoutWidgetsBinding(String routeName, {Object? extra}) {
+    while (Navigator.of(this).canPop()) {
+      back();
+    }
+    pushReplacementNamed(routeName, extra: extra);
+  }
+
+  Future<T?> navigateAndRemoveCurrent<T extends Object?>(String routeName, {Object? extra}) {
+    back();
+    return pushNamed(routeName, extra: extra);
+  }
+
+  /// Navigates to a named route and optionally provides extra arguments.
+  ///
+  /// This method pushes the specified [routeName] onto the navigator stack and
+  /// optionally passes [extra] arguments to the new route. It returns a
+  /// [Future] that completes to the result, if any, returned when the new route
+  /// is popped.
+  ///
+  /// - [T] is the type of the result expected when the new route is popped.
+  /// - [routeName] is the name of the route to navigate to.
+  /// - [extra] is an optional object to pass as extra arguments to the new route.
+  ///
+  /// Returns a [Future] that completes to the result when the new route is popped.
+  Future<T?> navigateWithPushNamed<T extends Object?>(String routeName, {Object? extra}) {
+    return pushNamed(routeName, extra: extra);
+  }
+
+  Future<T?> navigateWithPushNamedWithWidgetsBinding<T extends Object?>(String routeName, {Object? extra}) {
+    final Completer<T?> completer = Completer<T?>();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      pushNamed(routeName, extra: extra).then((value) {
+        completer.complete(value as T?);
+      });
+    });
+
+    return completer.future;
+  }
+}
