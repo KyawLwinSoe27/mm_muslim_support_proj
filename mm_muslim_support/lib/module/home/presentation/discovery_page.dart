dc8090debe9011eca0d34f07ab0cf660371f10a0
kyawlwinsoe <kyawlwinsoe@globalwave.company>
2025-05-04 01:23:19 +0630

    notification implementation

---
+++ mm_muslim_support/lib/module/home/presentation/discovery_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:mm_muslim_support/service/local_notification_service.dart';
+
+class DiscoveryPage extends StatefulWidget {
+  const DiscoveryPage({super.key});
+
+  @override
+  State<DiscoveryPage> createState() => _DiscoveryPageState();
+}
+
+class _DiscoveryPageState extends State<DiscoveryPage> {
+  List<NotiPayLoad> notiPayload = [];
+
+  void _loadNotifications() async {
+    final List<NotiPayLoad> pendingNotis = await LocalNotificationService().retrievePendingNotificationList();
+    setState(() {
+      notiPayload = pendingNotis;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Discovery")),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: _loadNotifications,
+            child: const Text('Get Scheduled Notifications'),
+          ),
+          const SizedBox(height: 16),
+          Expanded(
+            child: ListView.builder(
+              itemCount: notiPayload.length,
+              itemBuilder: (context, index) {
+                final noti = notiPayload[index];
+                return ListTile(
+                  title: Text('ID: ${noti.id}'),
+                  subtitle: Text(noti.name),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
