4c8ec65f7eab729bbd98529b5f951c650af2142f
Kyaw Lwin Soe <kyawlwinsoe@globalwave.company>
2025-05-03 08:02:53 +0630

    PrayerTime UI Implementation

--- mm_muslim_support/lib/module/home/presentation/namaz_times_page.dart
+++ mm_muslim_support/lib/module/home/presentation/namaz_times_page.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:mm_muslim_support/main.dart';
 import 'package:mm_muslim_support/model/custom_prayer_time.dart';
 import 'package:mm_muslim_support/module/home/cubit/change_date_cubit.dart';
 import 'package:mm_muslim_support/module/home/cubit/get_location_time_cubit/get_location_time_cubit.dart';
 import 'package:mm_muslim_support/module/home/cubit/get_prayer_time_cubit/get_prayer_time_cubit.dart';
 import 'package:mm_muslim_support/service/local_notification_service.dart';
-import 'package:mm_muslim_support/service/notification_service.dart';
-import 'package:mm_muslim_support/service/permission_service.dart';
 import 'package:mm_muslim_support/utility/extensions.dart';
 import 'package:mm_muslim_support/utility/image_constants.dart';
 
@@ -43,9 +39,7 @@ class NamazTimesPage extends StatelessWidget {
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                SizedBox(
-                                  width: 70,
-                                    child: Text('Prayer', style: TextStyle(fontWeight: FontWeight.bold))),
+                                SizedBox(width: 70, child: Text('Prayer', style: TextStyle(fontWeight: FontWeight.bold))),
                                 Text('Time', style: TextStyle(fontWeight: FontWeight.bold)),
                                 Text('Notify', style: TextStyle(fontWeight: FontWeight.bold)),
                               ],
@@ -83,48 +77,25 @@ class NamazTimesPage extends StatelessWidget {
                                               style: Theme.of(context).textTheme.titleMedium,
                                             ),
                                             ElevatedButton(
-                                              onPressed: () async{
-                                                final now = DateTime.now();
-                                                NotificationService.scheduleDailyReminder(
-                                                  hour: now.hour,
-                                                  minute: now.minute + 1, // ⏱ fire in 1 minute
-                                                  title: "Test Reminder",
-                                                  body: "This is a test notification",
-                                                  notificationId: now.microsecond,
-                                                );
-                                                ScaffoldMessenger.of(context).showSnackBar(
-                                                  const SnackBar(content: Text("Fajr reminder set!")),
-                                                );
-                                                flutterLocalNotificationsPlugin.pendingNotificationRequests().then((requests) {
-                                                  print("Pending notifications: ${requests.length}");
-                                                  for (var req in requests) {
-                                                    print("Notification: ${req.id} - ${req.title} at ${req.body}");
-                                                  }
-                                                });
-
-                                                await flutterLocalNotificationsPlugin.show(
-                                                  101,
-                                                  'Immediate Test',
-                                                  'This should play sound',
-                                                  const NotificationDetails(
-                                                    android: AndroidNotificationDetails(
-                                                      'daily_reminder_channel',
-                                                      'Test Notifications',
-                                                      importance: Importance.max,
-                                                      priority: Priority.high,
-                                                      playSound: true,
-                                                      sound: RawResourceAndroidNotificationSound('azan'), // or leave null
-                                                    ),
-                                                    iOS: DarwinNotificationDetails(
-                                                      presentSound: true,
-                                                      sound: 'azan.mp3',
-                                                    ),
-                                                  ),
-                                                );
-
+                                              onPressed: () async {
+                                                if (prayerTime.enableNotify) {
+                                                  LocalNotificationService().cancelNotificationById(prayerTime.id);
+                                                  context.read<GetPrayerTimeCubit>().toggleNotificationEnable(index, state.prayerTimes, false);
+                                                } else {
+                                                  DateTime now = DateTime.now();
+                                                  prayerTime.id = now.millisecond;
+                                                  LocalNotificationService().scheduleNotification(
+                                                    id: prayerTime.id,
+                                                    title: prayerTime.prayerName.toUpperCase(),
+                                                    body: "Don't for get to pray for ${prayerTime.prayerName}",
+                                                    hour: prayerTime.hour,
+                                                    minute: prayerTime.minute,
+                                                  );
+                                                  context.read<GetPrayerTimeCubit>().toggleNotificationEnable(index, state.prayerTimes, true);
+                                                }
                                               },
-                                              child: const Text("Set Fajr Reminder"),
-                                            )
+                                              child: prayerTime.enableNotify ? Icon(Icons.notifications_active, color: context.colorScheme.primary) : const Icon(Icons.notifications_active_outlined),
+                                            ),
                                           ],
                                         ),
                                       );
@@ -170,7 +141,6 @@ class PrayerDateWidget extends StatelessWidget {
               child: InkWell(
                 onTap: () async {
                   ChangeDateCubit changeDateCubit = context.read<ChangeDateCubit>();
-                  GetPrayerTimeCubit getPrayerTimeCubit = context.read<GetPrayerTimeCubit>();
                   DateTime? selectedDate = await showDatePicker(context: context, initialDate: context.read<ChangeDateCubit>().date, firstDate: DateTime(2000), lastDate: DateTime(2100));
                   if (selectedDate != null) {
                     changeDateCubit.setDate(selectedDate);
@@ -204,34 +174,31 @@ class LocationAndTimeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-  create: (context) => GetLocationTimeCubit()..getLocationTime(),
-  child: BlocBuilder<GetLocationTimeCubit, GetLocationTimeState>(
-  builder: (context, state) {
-    if(state is GetLocationTimeLoaded) {
-      return SizedBox(
-        height: 150,
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Location: ${state.location}', style: context.textTheme.titleMedium?.copyWith(color: context.colorScheme.onSecondary)),
-              const SizedBox(height: 10),
-              Text('Today\'s Date: ${state.date}', style: context.textTheme.titleSmall?.copyWith(color: context.colorScheme.onSecondary)),
-              const SizedBox(height: 10),
-              Text('Current Prayer: ${state.currentPrayer}', style: context.textTheme.titleSmall?.copyWith(color: context.colorScheme.onSecondary)),
-            ],
-          ),
-        ),
-      );
-    } else {
-      return const SizedBox(
-        height: 150,
-        child: Center(child: CircularProgressIndicator()),
-      );
-    }
-  },
-),
-);
+      create: (context) => GetLocationTimeCubit()..getLocationTime(),
+      child: BlocBuilder<GetLocationTimeCubit, GetLocationTimeState>(
+        builder: (context, state) {
+          if (state is GetLocationTimeLoaded) {
+            return SizedBox(
+              height: 150,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text('Location: ${state.location}', style: context.textTheme.titleMedium?.copyWith(color: context.colorScheme.onSecondary)),
+                    const SizedBox(height: 10),
+                    Text('Today\'s Date: ${state.date}', style: context.textTheme.titleSmall?.copyWith(color: context.colorScheme.onSecondary)),
+                    const SizedBox(height: 10),
+                    Text('Current Prayer: ${state.currentPrayer}', style: context.textTheme.titleSmall?.copyWith(color: context.colorScheme.onSecondary)),
+                  ],
+                ),
+              ),
+            );
+          } else {
+            return const SizedBox(height: 150, child: Center(child: CircularProgressIndicator()));
+          }
+        },
+      ),
+    );
   }
 }
 
--- mm_muslim_support/lib/service/local_notification_service.dart
+++ mm_muslim_support/lib/service/local_notification_service.dart
@@ -1,20 +1,91 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:mm_muslim_support/main.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:flutter_timezone/flutter_timezone.dart';
 
 class LocalNotificationService {
-  static Future<void> initializeNotifications() async {
-    const AndroidInitializationSettings androidSettings =
-    AndroidInitializationSettings('@mipmap/ic_launcher');
+  final notificationPlugin = FlutterLocalNotificationsPlugin();
 
-    const InitializationSettings initSettings = InitializationSettings(
-      android: androidSettings,
-      iOS: DarwinInitializationSettings(
-        requestAlertPermission: true,
-        requestBadgePermission: true,
-        requestSoundPermission: true,
-      ),
-    );
+  bool _isInitialized = false;
+  bool get isInitialized => _isInitialized;
 
-    await flutterLocalNotificationsPlugin.initialize(initSettings);
+  // INITIALIZE NOTIFICATION
+  Future<void> initNotification() async {
+    if (_isInitialized) return;
+
+    // init time zone handling
+    tz.initializeTimeZones();
+    final String timeZoneName = await FlutterTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(timeZoneName));
+
+    // init Android
+    const AndroidInitializationSettings initSettingAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    // init IOS
+
+    const DarwinInitializationSettings initSettingIOS = DarwinInitializationSettings(requestAlertPermission: true, requestBadgePermission: true, requestSoundPermission: true);
+
+    // init settings
+    const InitializationSettings initSettings = InitializationSettings(android: initSettingAndroid, iOS: initSettingIOS);
+
+    await notificationPlugin.initialize(initSettings);
+
+    _isInitialized = true;
   }
+
+  // Shown an immediate notification
+  Future<void> showNotification({int id = 0, String? title, String? body}) async {
+    await notificationPlugin.show(id, title, body, notificationDetail);
+  }
+
+  static AndroidNotificationDetails androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+    'your_channel_id',
+    'your_channel_name',
+    channelDescription: 'your_channel_description',
+    importance: Importance.max,
+    priority: Priority.max,
+    playSound: true,
+    visibility: NotificationVisibility.public,
+    audioAttributesUsage: AudioAttributesUsage.alarm,
+    sound: RawResourceAndroidNotificationSound('azan'),
+  );
+
+  static DarwinNotificationDetails iOSPlatformChannelSpecifics = const DarwinNotificationDetails(sound: 'azan.mp3');
+
+  NotificationDetails notificationDetail = NotificationDetails(android: androidPlatformChannelSpecifics, iOS: iOSPlatformChannelSpecifics);
+
+  // Schedule a notification
+  Future<void> scheduleNotification({required int id, required String title, required String body, required int hour, required int minute}) async {
+
+    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+    tz.TZDateTime scheduledDate = tz.TZDateTime(tz.local, now.year, now.month, now.day, hour, minute);
+
+    await notificationPlugin.zonedSchedule(
+      id,
+      title,
+      body,
+      scheduledDate,
+      notificationDetail,
+
+      // IOS specific: Use exact time specified
+      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+
+      // Android specific: Use exact time specified
+      androidScheduleMode: AndroidScheduleMode.alarmClock,
+
+      // Make the notification repeat daily
+      matchDateTimeComponents: DateTimeComponents.dayOfWeekAndTime,
+    );
+  }
+
+  // Cancel All notification
+  Future<void> cancelAllNotification() async {
+    await notificationPlugin.cancelAll();
+  }
+
+  // Cancel Notification By Id
+  Future<void> cancelNotificationById(int id) async{
+    await notificationPlugin.cancel(id);
+  }
+
 }
